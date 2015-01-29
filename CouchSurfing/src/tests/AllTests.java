d94413a370d003ddefcf17bcede65a12780e909c
Bastien <gargatuloth81@gmail.com>
2015-01-29 10:17:33 +0100

    Ajout de la classe data et rename d'une classe de test a la quelle il manquait une majuscule ^^

--- CouchSurfing/src/tests/AllTests.java
+++ CouchSurfing/src/tests/AllTests.java
@@ -5,7 +5,7 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ testFormulaireConnexion.class, TestFormulaireInscription.class,
+@SuiteClasses({ TestFormulaireConnexion.class, TestFormulaireInscription.class,
 		 TestHebergeur.class, TestLogement.class,
 		TestUtilisateur.class,TestFormulaireProposerLogement.class,TestFormulaireRechercheAnnonce.class })
 public class AllTests {
