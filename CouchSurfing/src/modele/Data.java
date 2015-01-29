5f9c1b205d9e16513f5f6f74860af2396321c1e8
Bastien <gargatuloth81@gmail.com>
2015-01-29 19:24:16 +0100

    Ajout du switch de BDD !!! (non testé car Hors ligne)

--- CouchSurfing/src/modele/Data.java
+++ CouchSurfing/src/modele/Data.java
@@ -8,5 +8,9 @@ public final class Data {
 	public static final String HOST_PWD = "teamBifle";
 	public static final String BDD_USER = "serveur";
 	public static final String BDD_PORT = "3306";
-	public static final Connection connection = ConnectionMySQL.getInstance(false);
+	public static Connection connection = ConnectionMySQL.getInstance(false);
+	
+	public static void switchBDD_or_BDDTest(boolean inTest){
+		Data.connection=ConnectionMySQL.getInstance(inTest);
+	}
 }
--- CouchSurfing/src/tests/AllTests.java
+++ CouchSurfing/src/tests/AllTests.java
@@ -1,5 +1,9 @@
 package tests;
 
+import modele.Data;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -16,5 +20,13 @@ import org.junit.runners.Suite.SuiteClasses;
 				TestPostule.class
 				})
 public class AllTests {
+	@Before
+	public void setUp() throws Exception {
+		Data.switchBDD_or_BDDTest(true);
+	}
 
+	@After
+	public void tearDown() throws Exception {
+		Data.connection.rollback();
+	}
 }
