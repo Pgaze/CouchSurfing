1b95444786018d1174440b709d3f474bf581f004
Bastien <gargatuloth81@gmail.com>
2015-02-04 11:03:05 +0100

    oops, BDD_USER = bastien => serveur

--- CouchSurfing/src/modele/Data.java
+++ CouchSurfing/src/modele/Data.java
@@ -6,7 +6,7 @@ public final class Data {
 
 	public static final String BDD_IP = "192.168.1.7";
 	public static final String BDD_PWD = "teamBifle";
-	public static final String BDD_USER = "bastien";
+	public static final String BDD_USER = "serveur";
 	public static final String BDD_PORT = "3306";
 	public static String BDD_NAME = "/CouchSurfing";
 	public static Connection BDD_Connection = ConnectionMySQL.getInstance();
