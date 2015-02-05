c83edf30e69b2746829249cf4e35564a74322421
Maxime <janvier.maxime.mj@gmail.com>
2015-02-05 12:12:06 +0100

    Tentative d'affichage de l'image dans la servlet. Commit avant test car besoin de pull

--- CouchSurfing/src/vue/Profil.java
+++ CouchSurfing/src/vue/Profil.java
@@ -1,6 +1,7 @@
 package vue;
 
 import java.io.IOException;
+import java.nio.file.Files;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import modele.Image;
 import modele.Logement;
 import modele.Utilisateur;
 import classes.Menu;
@@ -33,14 +35,27 @@ public class Profil extends HttpServlet {
 
 		if (request.getSession().getAttribute("sessionUtilisateur") != null) {
 			request.setAttribute("menu", Menu.getMenuMembre(request).getLiensMenu());
-			
 			Utilisateur user = (Utilisateur) request.getSession().getAttribute("sessionUtilisateur");
-			
 			try {
+				//Recuperation et affichage de l'image de profil
+				int idImageProfil = user.getIdPhotoProfil();
+				Image image;
+				if(idImageProfil!=-1){
+					image = Image.getImageById(0);
+				}
+				else{
+					 image = Image.getImageById(idImageProfil);
+				}
+				byte[] img = Files.readAllBytes(image.getImage().toPath());
+				response.setContentType("png");
+				response.setContentLength(img.length);
+				response.getOutputStream().write(img);
+				
+				//Affichage de l'adresse du logmet si existant
 				Logement logementUtilisateur = Logement.getLogementById(user.getIdLogement());
 				if(Logement.getLogementById(user.getIdLogement()) != null){
 					request.setAttribute("addresseLogement",logementUtilisateur.getAdresse().toString());
-				}request.setAttribute("adresseLogement","<p>Vous n'avez pas de logement enregistré. <a href='nouvelle'>Créez en un !</a></p>");
+				}request.setAttribute("adresseLogement","<p>Vous n'avez pas de logement enregistrÃ©. <a href='nouvelle'>CrÃ©ez en un !</a></p>");
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
