36ef85312ae24bf6abed917d3d438cdc7f3b36c2
Bastien <gargatuloth81@gmail.com>
2015-02-24 17:02:16 +0100

    Gestion des dates lors de la création de l'objet Offre (pas de gestion de la validité des offres dans le temps)

--- CouchSurfing/src/modele/FormulaireRechercheAnnonce.java
+++ CouchSurfing/src/modele/FormulaireRechercheAnnonce.java
@@ -14,20 +14,20 @@ public class FormulaireRechercheAnnonce {
 	}
 	
 	/**
-	 * @return liste des offres pour une ville donnée
+	 * @return liste des offres pour une ville donnï¿½e
 	 * @throws Exception
 	 */
 	public List<Offre> getListeOffre() throws Exception {
 		List<Offre> result = new ArrayList<Offre>();
 		PreparedStatement s = Data.BDD_Connection.prepareStatement(
-					"select Logement.IdLogement,Utilisateur.IdUtilisateur from Utilisateur,Logement "
+					"select Logement.IdLogement,Utilisateur.IdUtilisateur,Logement.DateDebut,Logement.DateFin from Utilisateur,Logement "
 					+ "where Logement.IdLogement=Utilisateur.IdLogement and Logement.ville=?");
 		s.setString(1, this.ville);
 		ResultSet rs=s.executeQuery();
 		while (rs.next()){
 			Logement l=Logement.getLogementById(rs.getInt(1));
 			Utilisateur u=Utilisateur.getUtilisateurById(rs.getInt(2));
-			result.add(new Offre(l, u, "01-01-1901", "01-01-1901"));
+			result.add(new Offre(l, u, rs.getString(3), rs.getString(4)));
 		}
 		if (result.isEmpty()){
 			throw new Exception("Aucun logement a "+this.ville);
--- CouchSurfing/src/modele/Offre.java
+++ CouchSurfing/src/modele/Offre.java
@@ -1,7 +1,10 @@
 package modele;
 
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+
+import javax.naming.directory.InvalidAttributeValueException;
 
 public class Offre {
 
@@ -9,13 +12,63 @@ public class Offre {
 	private Utilisateur hebergeur;
 	private String dateDebut,dateFin;
 	
+	/**
+	 * @param logement
+	 * @param hebergeur
+	 * @param dateDebut yyyy-mm-dd
+	 * @param dateFin yyyy-mm-dd
+	 */
 	public Offre(Logement logement, Utilisateur hebergeur, String dateDebut, String dateFin) {
 		this.setLogement(logement);
 		this.setHebergeur(hebergeur);
-		this.setDateDebut(dateDebut);
-		this.setDateFin(dateFin);
+		if(dateDebut != null && dateFin != null){
+			try {
+				checkIntegriteDates(dateDebut,dateFin);
+				this.setDateDebut(dateDebut);
+				this.setDateFin(dateFin);
+			} catch (InvalidAttributeValueException | IllegalArgumentException e) {
+				this.setDateDebut(null);
+				this.setDateFin(null);
+			}
+		}		
 	}
-	
+
+	/** Verifie l'integrite des dates debut et fin renseignées
+	 * @param strDateDebut
+	 * @param strDateFin
+	 * @throws InvalidAttributeValueException
+	 */
+	private void checkIntegriteDates(String strDateDebut, String strDateFin) throws InvalidAttributeValueException {
+		//reformatage de la date
+		Date newDateDebut,newDateFin;
+		newDateDebut = Date.valueOf(strDateDebut);
+		newDateFin = Date.valueOf(strDateFin);
+		//vérifications
+		if(!newDateFin.after(newDateDebut))
+			throw new InvalidAttributeValueException("La date de fin n'est pas postérieure a la date de début");
+		Date dateCourante = new Date(System.currentTimeMillis());
+		if(newDateDebut.equals(dateCourante) || newDateDebut.before(dateCourante))
+			throw new InvalidAttributeValueException("La date de debut est antérieure ou égale a la date actuelle");
+	}
+
+
+	/**
+	 * @param year
+	 * @param month
+	 * @param day
+	 * @return "yyyy-mm-dd"
+	 * @throws InvalidAttributeValueException
+	 */
+	public static String creerStringDate(int year, int month, int day) throws InvalidAttributeValueException {
+		String res = null;
+		//vérification sommaire
+		if(month <= 0 || day <= 0 || year <= 0 || day > 31 || month > 12){
+			throw new InvalidAttributeValueException("La date avant changement de format est invalide");
+		}else{
+			res = "" + year + "-" + month + "-" + day;
+		}
+		return res;
+	}
 
 	public Logement getLogement() {
 		return logement;
@@ -51,7 +104,7 @@ public class Offre {
 
 
 	public static Offre getOffreByIdLogement(int idLogement) throws Exception {
-		String sql = "SELECT Utilisateur.idUtilisateur,Logement.idLogement,DateDebut,DateFin "
+		String sql = "SELECT Utilisateur.idUtilisateur,DateDebut,DateFin "
 				+ "FROM Logement,Utilisateur "
 				+ "WHERE Utilisateur.IdLogement=Logement.IdLogement "
 				+ "AND Logement.IdLogement=?";
@@ -59,10 +112,8 @@ public class Offre {
 		select.setInt(1, idLogement);
 		ResultSet res=select.executeQuery();
 		if(res.next()){
-			return new Offre(Logement.getLogementById(idLogement), 
-					Utilisateur.getUtilisateurById(res.getInt(1)), res.getString(3), res.getString(4));
-		}
-		else{
+			return new Offre(Logement.getLogementById(idLogement), Utilisateur.getUtilisateurById(res.getInt(1)), res.getString(2), res.getString(3));
+		}else{
 			return null;
 		}
 	}
