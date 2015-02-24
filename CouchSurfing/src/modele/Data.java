a77a36f38a473649a03224e4d67ba7304c752f0d
Maxime <janvier.maxime.mj@gmail.com>
2015-02-24 14:53:31 +0100

    On peut desormais afficher la page de profil de n'importe quelle utilisateur. Ajout du lien vers le profil sur l'annonce dans le resultat de la recherche

--- CouchSurfing/src/modele/Data.java
+++ CouchSurfing/src/modele/Data.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 
 public final class Data {
 
-	public static final String BDD_IP = "169.254.9.164";
+	public static final String BDD_IP = "192.168.1.17";
 	public static final String BDD_PWD = "teamBifle";
 	public static final String BDD_USER = "serveur";
 	public static final String BDD_PORT = "3306";
